945b743c14ef6888503edb4266cc825753f74393
ramkumar-nallusamy <rapidram507@gmail.com>
2020-12-12 13:14:42 -0800

    initial commit

--- src/app/main-components/home/home.component.ts
+++ src/app/main-components/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import { ChartType, ChartOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-home',
@@ -28,5 +31,48 @@ export class HomeComponent implements OnInit {
     },
     nav: true
   }
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      position: 'top',
+    },
+    plugins: {
+      datalabels: {
+        formatter: (value:any, ctx:any) => {
+          const label = ctx.chart.data.labels[ctx.dataIndex];
+          return label;
+        },
+      },
+    }
+  };
+  public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
+  public pieChartData: number[] = [300, 500, 100];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [pluginDataLabels];
+  public pieChartColors = [
+    {
+      backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
+    },
+  ];
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
 
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+
+  addSlice(): void {
+    this.pieChartLabels.push(['Line 1', 'Line 2', 'Line 3']);
+    this.pieChartData.push(400);
+    this.pieChartColors[0].backgroundColor.push('rgba(196,79,244,0.3)');
+  }
+
+  removeSlice(): void {
+    this.pieChartLabels.pop();
+    this.pieChartData.pop();
+    this.pieChartColors[0].backgroundColor.pop();
+  }
 }
